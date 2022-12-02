9c7310393ecddd8e3ef30a333ff169fd5e3acd33
ravenstorm2001 <64809368+ravenstorm2001@users.noreply.github.com>
2022-12-02 05:31:57 +0000

    Update address.py

--- fynesse/address.py
+++ fynesse/address.py
@@ -24,7 +24,7 @@ def predict_price(conn, latitude, longitude, date, property_type, size):
     """
     
     # Turning warnings off
-    warnings.filterwarnings("ignore")
+    warnings.filterwarnings("ignore", category=UserWarning)
     
     # Fetch Data from Database
     date_list = date.split('-')
@@ -50,7 +50,7 @@ def predict_price(conn, latitude, longitude, date, property_type, size):
     # Train Model with Features
     if data_gdf[data_gdf.property_type == property_type].shape[0] != 0 :
         data_gdf = data_gdf[data_gdf.property_type == property_type]
-    print(data_gdf[['closest_leisure', 'amenity_proximity']])
+    print(data_gdf)
     y = data_gdf['price']
     design = np.ones(data_gdf.shape[0]).reshape(-1,1)
     if 'amenity' in pois.columns:
@@ -73,6 +73,7 @@ def predict_price(conn, latitude, longitude, date, property_type, size):
         closest_leisure_pred = pois_leisure.geometry.distance(gdf_pred.geometry[0]).min()
         design_pred = np.concatenate((design_pred, [[closest_leisure_pred]]),axis=1)
     y_pred_linear_basis = results_basis.get_prediction(design_pred).summary_frame(alpha=0.05)
+    print(desing_pred)
     print(results_basis.summary())
     print(y_pred_linear_basis)
     pass
